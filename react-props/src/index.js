9bf8b345b6d67716ad0cc6849f6e42617ef531cf
pavelFaraday <devstudio.corp@gmail.com>
2021-07-01 12:43:56 +0400

    Special Key Prop

--- react-props/src/index.js
+++ react-props/src/index.js
@@ -6,12 +6,14 @@ import './index.css'
 // ----------------------  SECOND WAY ------------------------ //
 const books = [
   {
+    id: 1,
     img: 'https://images.pexels.com/photos/1289845/pexels-photo-1289845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     altText: 'Eliphant',
     title: 'The Picture of Eliphant',
     author: 'Emilie Robertson'
   }, 
   {
+    id: 2,
     img: 'https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG9ncmFwaHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
     altText: 'Photo',
     title: 'Take Photo',
@@ -19,21 +21,17 @@ const books = [
   }
 ]
 
-
 function BookList () {
   return (
     <section className='bookList'>
-      {books.map(book => {                        {/*MAPING*/}
-        const {img, title, author} = book;             {/* Destructuring */}
+      {books.map(book => {                                    
         return (
-          <Book book={book}/>
+          <Book key={book.id} book={book}/>
         )
       })}
     </section>
   ) 
 }
-
-
 
 const Book = (props) => {
   const {img, altText, title, author} = props.book
@@ -45,51 +43,10 @@ const Book = (props) => {
          />
         <h3>{title}</h3>
         <h6>{author}</h6>
-        {props.children} {/*children props*/}
     </article>
   )
 }
-// --------------------------  SECOND WAY -------------------------- //
 
-
-// function BookList () {
-//   return (
-//     <section className='bookList'>
-//       <Book 
-//         img='https://images.pexels.com/photos/1289845/pexels-photo-1289845.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-//         altText = 'Eliphant'
-//         title = 'The Picture of Eliphant'
-//         author = 'Emilie Robertson'
-//       > 
-//       {/* Children Props */}
-//       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero molestias quo commodi. Optio doloremque, ad esse minus veniam laborum dicta?</p>
-      
-//       </Book> // we need close
-        
-//       <Book 
-//         img='https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG9ncmFwaHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
-//         altText = 'Photo'
-//         title = 'Take Photo'
-//         author = 'John Doe'
-//       />
-//     </section>
-//   ) 
-// }
-
-// const Book = (props) => {
-//   const { img, altText, title, author } = props
-//   return (
-//     <article className='book'>
-//         <img 
-//           src={props.img}
-//           alt={props.altText}
-//          />
-//         <h3>{props.title}</h3>
-//         <h6>{props.author}</h6>
-//         {props.children} {/*children props*/}
-//     </article>
-//   )
-// }
 
 ReactDom.render(
   <BookList />, document.getElementById('root')
