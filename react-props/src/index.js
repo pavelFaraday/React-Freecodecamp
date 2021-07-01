7ec751292144858de7b84d26537fe18b81cfe8c5
pavelFaraday <devstudio.corp@gmail.com>
2021-07-01 13:41:34 +0400

    React Events

--- react-props/src/index.js
+++ react-props/src/index.js
@@ -35,14 +35,29 @@ function BookList () {
 
 const Book = (props) => {
   const {img, altText, title, author} = props
+
+  const onclickHandler = () => {
+    alert('I am Event');
+  }
+
+  const example = (author) => {
+    console.log(author);
+  } 
+
   return (
-    <article className='book'>
+    <article 
+      className='book' 
+      onMouseOver={() => {
+        console.log(title)
+    }}>
         <img 
           src={img}
           alt={altText}
          />
         <h3>{title}</h3>
         <h6>{author}</h6>
+        <button type='button' onClick={onclickHandler}>ClickMe</button>
+        <button type='button' onClick={() => example(author)}>clickToo</button>
     </article>
   )
 }
