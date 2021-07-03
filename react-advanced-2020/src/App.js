837a1865db2568e85d4f825000dfbf06746acc3d
pavelFaraday <devstudio.corp@gmail.com>
2021-07-03 16:49:14 +0400

    useState Hook with nums - Simple Counter Project

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import UseStateObject from './tutorial/1-useState/final/4-useState-object'
+import UseStateCounter from './tutorial/1-useState/final/5-useState-counter'
 
 
 function App() {
   return (
     <div className='container'>
-      <UseStateObject />
+      <UseStateCounter />
     </div>
   )
 }
