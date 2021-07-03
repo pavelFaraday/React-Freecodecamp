301856ec12b5bd03d88c440103f998bcee2bb60a
pavelFaraday <devstudio.corp@gmail.com>
2021-07-03 15:07:27 +0400

    useState Hook with  an Array

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import UseStateBasics from './tutorial/1-useState/final/2-useState-basics';
-
+import UseStateArray from './tutorial/1-useState/final/3-useState-array'
 
 
 function App() {
   return (
     <div className='container'>
-      <UseStateBasics />
+      <UseStateArray />
     </div>
   )
 }
--- react-advanced-2020/src/tutorial/1-useState/final/3-useState-array.js
+++ react-advanced-2020/src/tutorial/1-useState/final/3-useState-array.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { data } from '../../../data';
+
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
+  // Remove individual item from an array
+  // Meaneng: Leave all elements that doesnt have certain id
   const removeItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+
   return (
     <>
       {people.map((person) => {
         const { id, name } = person;
+
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
@@ -18,11 +23,14 @@ const UseStateArray = () => {
           </div>
         );
       })}
+
+      {/* Clear all Array item - make Array empty*/}
       <button className='btn' onClick={() => setPeople([])}>
         clear items
       </button>
     </>
   );
+
 };
 
 export default UseStateArray;
