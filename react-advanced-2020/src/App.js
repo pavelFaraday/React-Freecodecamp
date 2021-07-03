e413e8e4798d47b9862bb600b87d34842e180f6b
pavelFaraday <devstudio.corp@gmail.com>
2021-07-03 12:26:30 +0400

    useState Hook - ERROR message | Why we cant change/rerender value of 'title' propery without useState Hook |

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ErrorExample from './tutorial/1-useState/final/1-error-example';
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <ErrorExample />
     </div>
   )
 }
--- react-advanced-2020/src/tutorial/1-useState/final/1-error-example.js
+++ react-advanced-2020/src/tutorial/1-useState/final/1-error-example.js
@@ -3,10 +3,14 @@ import React from 'react';
 const ErrorExample = () => {
   let title = 'random title';
 
+  // There React cant change/rerender value of 'title' propery without useState Hook !
+  // It can only change value in console!
+  
   const handleClick = () => {
     title = 'hello people';
     console.log(title);
   };
+
   return (
     <React.Fragment>
       <h2>{title}</h2>
