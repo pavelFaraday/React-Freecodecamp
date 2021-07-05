ae756e13d7081833b6012677113b54918f05c908
pavelFaraday <devstudio.corp@gmail.com>
2021-07-05 13:08:56 +0400

    1-useEffect-basics  |   Second argument in useEffect - list of dependences

--- react-advanced-2020/src/tutorial/2-useEffect/final/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/final/1-useEffect-basics.js
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from 'react';
 
 // useEffect allows you to do a side effects (work outside a component !). That work could be: changing document title, sighup for subscription, fetching data, settingup event listener, etc. By default function inside useEffect runs after every re-render !!! Just this is power of useEffect.
 // GENERAL RULL: We cannot use Hooks inside Conditions ! Instead, We can place Condition inside Hook
+// You can have as many useEffects as you want !!!
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
+
+  
   useEffect(() => {
     console.log('call useEffect');                // it runs after render
     if (value > 0) {
       document.title = `New Messages(${value})`;  // it runs after render
     }
-  });
+  }, [value]);  // Array Brackets means, second argument - lists of dependences. If you pass there some argument. If this argument will change, the function inside useEffect will run. So, useEffect funtionality depends on this argument. 
+
+
+
+  // useEffect(() => {
+  //   console.log('call useEffect');                // it runs after render
+  //   if (value > 0) {
+  //     document.title = `New Messages(${value})`;  // it runs after render
+  //   }
+  // }, []);  // Array Brackets means, second argument - lists of dependences. IN THIS CASE, function inside a useEffect run only once - on the INITIAL RENDER. It will not run second or third time after each RERENDERING! ONLY ONCE!
+
 
   console.log('render component');  // render runnig
   return (
